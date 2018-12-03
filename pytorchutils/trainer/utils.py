00afcdb975ff7c98586553c435b367983761cef8
Stone Ye <dong100136@outlook.com>
2018-12-03 19:14:39 +0800

    add trainer lr

--- pytorchutils/trainer/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def check_and_create_dir(path):
-    if not os.path.exists(path):
-        if not os.path.exists(os.path.dirname(path)):
-            check_and_create_dir(os.path.dirname(path))
-        print("create dir %s" % path)
-        os.mkdir(path)
